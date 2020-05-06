49ae33b9cf2e3c7313a2756eb3de1cf1d99e8d9c
Minsong Lee <minsong0313@naver.com>
2020-05-06 12:38:09 +0900

    googlemap2- ex1

--- app/src/main/java/com/example/googlemap2/MainActivity.java
+++ app/src/main/java/com/example/googlemap2/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.googlemap2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -13,6 +15,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -30,10 +33,12 @@ import com.google.android.gms.tasks.OnSuccessListener;
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
 
+    private static final int REQUEST_CODE_PERMISSIONS = 1000;
     private GoogleMap mMap = null;
     private GoogleApiClient mGoogleApiClient = null;
 
     private FusedLocationProviderClient  mFusedLocationClient;
+    boolean canReadLocation = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,8 +57,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap =  googleMap;
 
-        LatLng univer = new LatLng(36.833773, 127.179262);
-        mMap.addMarker(new MarkerOptions().position(univer).title("상명대학교"));
+        LatLng univer = new LatLng(37.553439, 126.972657);
+        mMap.addMarker(new MarkerOptions().position(univer).title("서울역"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(univer));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(17));//1~21
 
@@ -70,11 +75,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }//상명대학교 위도, 경도로 마커 찍기
 
     public void onLastLocationButtonClicked(View view) {//현재 위치 정보 얻어오기
-
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager
+                .PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+        PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_CODE_PERMISSIONS);
+            return;
+        }
         mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>(){
             @Override
             public void onSuccess(Location location) {
-                if (location != null){
+                if (location != null){//마지막 위치 가져오기
                     LatLng mylocation = new LatLng(location.getLatitude(), location.getLongitude());
                     mMap.addMarker(new MarkerOptions()
                         .position(mylocation)
@@ -85,5 +97,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode){
+            case REQUEST_CODE_PERMISSIONS:
+                if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager
+                        .PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "권한 체크 거부 됨", Toast.LENGTH_SHORT).show();
+                }
+
+        }
     }
 }
